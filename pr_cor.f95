febeee4ff358e41d132c98438683339cbe2dee4a
misha227 <55085713+misha227@users.noreply.github.com>
2019-09-12 22:48:01 +0300

    Add files via upload

--- pr_cor.f95
+++ pr_cor.f95
@@ -1,32 +1,20 @@
 program pr_cor
 implicit none
-real x,y,r,fi,pi
+real(8) x,y,r,fi,pi
+
     pi= 3.141592653589793
     write(*,*) 'Add variable X'
     read(*,*) x
     write(*,*) 'Add variable Y'
     read(*,*) y
     r= sqrt(x**2 + y**2)
-    if (x < 0) then
-        fi= pi + atan(y/x)
-    end if
-    if (x == 0) then
-        if (y < 0) then
-            fi= (3*pi)/2
-        else
-            if (y == 0) then
-                fi= 0
-            else
-                if (y > 0) then
-                    fi= pi/2
-                end if
-            end if
-        end if
-    end if
-    if (y >= 0) then
-        fi= atan(y/x)
-    else
-        fi= 2*pi + atan(y/x)
-    end if
+    
+    if ((x .EQ. 0) .AND. (y .EQ. 0)) fi= 0
+    if ((x .EQ. 0) .AND. (y .GT. 0)) fi= pi/2
+    if ((x .EQ. 0) .AND. (y .LT. 0)) fi= (3*pi)/2
+    if ((x .GT. 0) .AND. (y .GE. 0)) fi= ATAN(y/x)
+    if ((x .GT. 0) .AND. (y .LT. 0)) fi= 2*pi + ATAN(y/x)
+    if  (x .LT. 0)                   fi= pi + ATAN(y/x)
+    
     write(*,*) 'r=', r, 'fi=', fi
 end program pr_cor
