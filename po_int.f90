2449fd650e36f0f793ec142f3a9888b00edab373
misha227 <55085713+misha227@users.noreply.github.com>
2019-09-14 19:57:19 +0300

    Update po_int.f90

--- po_int.f90
+++ po_int.f90
@@ -1,10 +1,10 @@
-program po_int                        !rvl = radius vector length
-implicit none                         !mv  = max value
-real(8) x(1:6), y(1:6), rvl(1:6), mv  !mvi = max value index
+program po_int                           !rvl = radius vector length
+implicit none                            !mv  = max value
+real(8) x(1:13), y(1:13), rvl(1:13), mv  !mvi = max value index
 integer i, mvi
 
     i= 1                              !unit fill the array
-    do while(i .LE. 6)
+    do while(i .LE. 13)
         write(*,*) 'add X of point P with index', i
         read(*,*) x(i)
         write(*,*) 'add Y of point P with index', i
@@ -13,14 +13,14 @@ integer i, mvi
     end do
     
     i= 1                              !remove the points in the 1, 2 and 4 quadrants
-    do while(i .LE. 6)
+    do while(i .LE. 13)
         if(x(i) .GT. 0) x(i)= 0
         if(y(i) .GT. 0) y(i)= 0
         i= i + 1
     end do
     
     i= 1                              !find the length radius vectors of the points
-    do while(i .LE. 6)
+    do while(i .LE. 13)
         rvl(i)= sqrt(x(i)**2 + y(i)**2)
         i= i + 1
     end do
@@ -28,7 +28,7 @@ integer i, mvi
     mv= MAXVAL(rvl)                   !find the farthest from the O(0,0) point
     
     i=1                               !find the index of the farthest point                             
-    do while(i .LE. 6)
+    do while(i .LE. 13)
         if(rvl(i) .EQ. mv) mvi= i
         i= i + 1
     end do
