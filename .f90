136f99704716482b3007c3ae886f56ec990b10b2
misha227 <55085713+misha227@users.noreply.github.com>
2019-09-19 18:07:54 +0300

    Add files via upload

--- .f90
+++ .f90
@@ -1,39 +1,35 @@
-program po_int                        !rvl = radius vector length
-implicit none                         !mv  = max value
-real(8) x(1:6), y(1:6), rvl(1:6), mv  !mvi = max value index
-integer i, mvi
-
-    i= 1                              !unit fill the array
-    do while(i .LE. 6)
-        write(*,*) 'add X of point P with index', i
-        read(*,*) x(i)
-        write(*,*) 'add Y of point P with index', i
-        read(*,*) y(i)
-        i= i + 1
+program p96ex33
+implicit none 
+real(16) g, gx0, xm, xm1, e0, x0, e 
+integer m 
+logical(1) L
+    
+    e= 0.0000001
+    write(*,*) e
+    x0= 4.7
+    gx0= sin(x0) - (x0 * cos(x0))
+    
+    xm= x0
+    L= .true.
+    m= 1
+    do while(L)
+        g= sin(xm) - (xm * cos(xm))
+        xm1= xm - (g / gx0)
+        e0= abs(xm1 - xm)
+        
+        write(*,*) e0, m
+        
+        L= e0 .GT. e
+        
+        if(.NOT. L) write(*,*) 'e0', e0
+        if(.NOT. L) write(*,*) 'm', m
+        if(.NOT. L) write(*,*) 'xm1', xm1
+        if(.NOT. L) g= sin(xm1) - (xm1 * cos(xm1))
+        if(.NOT. L) write(*,*) 'g', g
+        
+        xm= xm1
+        m= m + 1
+        !if(m .GT. 40) L= .false.
     end do
+end program p96ex33
     
-    i= 1                              !remove the points in the 1, 2 and 4 quadrants
-    do while(i .LE. 6)
-        if(x(i) .GT. 0) x(i)= 0
-        if(y(i) .GT. 0) y(i)= 0
-        i= i + 1
-    end do
-    
-    i= 1                              !find the length radius vectors of the points
-    do while(i .LE. 6)
-    rvl(i)= sqrt(x(i)**2 + y(i)**2)
-    i= i + 1
-    end do
-    
-    mv= MAXVAL(rvl)                   !find the farthest from the O(0,0) point
-    
-    i=1                               !find the index of the farthest point                             
-    do while(i .LE. 6)
-        if(rvl(i) .EQ. mv) mvi= i
-        i= i + 1
-    end do
-    
-    write(*,*) 'point P with index', mvi 
-    write(*,*) 'is from the O(0,0) at a distance of', mv
-    
-end program po_int
