0ce4dca402ec957508543d9f6cf4c5b8db809ac7
misha227 <55085713+misha227@users.noreply.github.com>
2019-09-19 18:12:24 +0300

    Add files via upload

--- .f90
+++ .f90
@@ -1,32 +1,31 @@
 program p96ex33
 implicit none 
-real(16) g, gx0, xm, xm1, e0, x0, e 
+real(16) g, g1x0, xm, xm1, e0, x0, e 
 integer m 
 logical(1) L
     
     e= 0.0000001
-    write(*,*) e
     x0= 4.7
-    gx0= sin(x0)
-    
+    g1x0= sin(x0)
     xm= x0
     L= .true.
     m= 1
     do while(L)
         g= sin(xm) - (xm * cos(xm))
-        xm1= xm - (g / gx0)
+        xm1= xm - (g / g1x0)
         e0= abs(xm1 - xm)
         
         write(*,*) e0, m
         
         L= e0 .GT. e
         
-        if(.NOT. L) write(*,*) 'e0', e0
-        if(.NOT. L) write(*,*) 'm', m
-        if(.NOT. L) write(*,*) 'xm1', xm1
-        if(.NOT. L) g= sin(xm1) - (xm1 * cos(xm1))
-        if(.NOT. L) write(*,*) 'g', g
-        
+        if(.NOT. L) then
+            write(*,*) 'e0', e0
+            write(*,*) 'm', m
+            write(*,*) 'xm1', xm1
+            g= sin(xm1) - (xm1 * cos(xm1))
+            write(*,*) 'g', g
+        end if
         xm= xm1
         m= m + 1
     end do
