1ffafd702f35f2b0a433c52c9c04eaf17c1cade3
misha227 <55085713+misha227@users.noreply.github.com>
2019-09-23 12:34:31 +0300

    Update .f90

--- .f90
+++ .f90
@@ -10,6 +10,7 @@ logical(1) L
     xm= x0
     L= .true.
     m= 1
+    
     do while(L)
         g= sin(xm) - (xm * cos(xm))
         xm1= xm - (g / g1x0)
@@ -19,15 +20,14 @@ logical(1) L
         
         L= e0 .GT. e
         
-        if(.NOT. L) then
-            write(*,*) 'e0', e0
-            write(*,*) 'm', m
-            write(*,*) 'xm1', xm1
-            g= sin(xm1) - (xm1 * cos(xm1))
-            write(*,*) 'g', g
-        end if
         xm= xm1
         m= m + 1
     end do
-end program p96ex33
     
+    write(*,*) 'e0', e0
+    write(*,*) 'm', m
+    write(*,*) 'xm1', xm1
+    g= sin(xm1) - (xm1 * cos(xm1))
+    write(*,*) 'g', g        
+    
+end program p96ex33
